a490c15a800b1e79c204d6051014a9e8e4ffe9eb
Helmi Amirudin <helmilaw@gmail.com>
2022-03-12 10:31:54 +0700

    test update some fixes

--- scripts/immortalwrt.sh
+++ scripts/immortalwrt.sh
@@ -34,8 +34,16 @@ rm -rf helmiwrt-packages/luci-app-v2raya
 svn co https://github.com/helmiau/helmiwrt-adds/trunk/packages/net/telegrambot helmiwrt-adds/telegrambot
 svn co https://github.com/helmiau/helmiwrt-adds/trunk/luci/luci-app-telegrambot helmiwrt-adds/luci-app-telegrambot
 
+# Add LuCI v2rayA
+git clone --depth=1 https://github.com/zxlhhyccc/luci-app-v2raya
+
 # Add luci-theme-neobird theme
 git clone --depth=1 https://github.com/helmiau/luci-theme-neobird
+
+# Add themes from kenzok8 openwrt-packages
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial_new kenzok8/luci-theme-atmaterial_new
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-edge kenzok8/luci-theme-edge
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-ifit kenzok8/luci-theme-ifit
 
 popd
 
@@ -51,3 +59,40 @@ popd
 
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+
+#-----------------------------------------------------------------------------
+#   Start of @helmiau terminal scripts additionals menu
+#-----------------------------------------------------------------------------
+HWOSDIR="package/base-files/files"
+rawgit="https://raw.githubusercontent.com"
+
+# Add vmess creator account from racevpn.com
+# run "vmess" using terminal to create free vmess account
+# wget --show-progress -qO $HWOSDIR/bin/vmess "$rawgit/ryanfauzi1/vmesscreator/main/vmess"
+
+# Add ram checker from wegare123
+# run "ram" using terminal to check ram usage
+wget --show-progress -qO $HWOSDIR/bin/ram "$rawgit/wegare123/ram/main/ram.sh"
+
+# Add fix download file.php for xderm and libernet
+# run "fixphp" using terminal for use
+wget --show-progress -qO $HWOSDIR/bin/fixphp "$rawgit/helmiau/openwrt-config/main/fix-xderm-libernet-gui"
+
+# Add wegare123 stl tools
+# run "stl" using terminal for use
+usergit="wegare123"
+mkdir -p $HWOSDIR/root/akun $HWOSDIR/usr/bin
+wget --show-progress -qO $HWOSDIR/usr/bin/stl "$rawgit/$usergit/stl/main/stl/stl.sh"
+wget --show-progress -qO $HWOSDIR/usr/bin/gproxy "$rawgit/$usergit/stl/main/stl/gproxy.sh"
+wget --show-progress -qO $HWOSDIR/usr/bin/autorekonek-stl "$rawgit/$usergit/stl/main/stl/autorekonek-stl.sh"
+wget --show-progress -qO $HWOSDIR/root/akun/tunnel.py "$rawgit/$usergit/stl/main/stl/tunnel.py"
+wget --show-progress -qO $HWOSDIR/root/akun/ssh.py "$rawgit/$usergit/stl/main/stl/ssh.py"
+wget --show-progress -qO $HWOSDIR/root/akun/inject.py "$rawgit/$usergit/stl/main/stl/inject.py"
+
+# Add wifi id seamless autologin by kopijahe
+# run "kopijahe" using terminal for use
+wget --show-progress -qO $HWOSDIR/bin/kopijahe "$rawgit/kopijahe/wifiid-openwrt/master/scripts/kopijahe"
+
+#-----------------------------------------------------------------------------
+#   End of @helmiau terminal scripts additionals menu
+#-----------------------------------------------------------------------------
--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -15,6 +15,69 @@ uci set luci.main.mediaurlbase='/luci-static/argon'
 # Disable IPV6 ula prefix
 sed -i 's/^[^#].*option ula/#&/' /etc/config/network
 
+# Set Timezone to Asia/Jakarta
+uci set system.@system[0].timezone='WIB-7'
+uci set system.@system[0].zonename='Asia/Jakarta'
+uci commit system
+
+# Bye-bye zh_cn
+opkg remove $(opkg list-installed | grep zh-cn)
+
+# Add ram checker from wegare123
+# run "ram" using terminal to check ram usage
+chmod +x /bin/ram
+
+# Add fix download file.php for xderm and libernet
+# run "fixphp" using terminal for use
+chmod +x /bin/fixphp
+
+# Add IP Address Info Checker
+# run "myip" using terminal for use
+chmod +x /bin/myip
+
+# Add Samba Allowed Guest Setup
+# run "sambaset" using terminal to set it up
+chmod +x /bin/sambaset
+
+# Add refresh IP Address for QMI Modems, such as LT4220
+# Script by Rudi Hartono https://www.facebook.com/rud18
+chmod +x /bin/ipqmi
+
+# Fix luci-app-atinout-mod by 4IceG
+chmod +x /usr/bin/luci-app-atinout
+
+# Fix for xderm mini gui if trojan is not installed
+ln -sf /usr/sbin/trojan /usr/bin/trojan
+
+# HelmiWrt Patches
+chmod +x /bin/helmiwrt
+#helmiwrt
+
+# Add default interfaces
+helmiwrt addinterfacefirewall
+
+# Set default theme to luci-theme-argon
+# Delete default watchcat setting
+# Set Google DNS as default DNS Forwarding
+cat << 'EOF' > /bin/default-theme
+
+uci set luci.main.mediaurlbase='/luci-static/argon'
+uci commit luci
+
+uci add_list dhcp.@dnsmasq[0].server='8.8.8.8'
+uci add_list dhcp.@dnsmasq[0].server='8.8.4.4'
+uci commit dhcp
+/etc/init.d/dnsmasq restart
+
+EOF
+chmod +x /bin/default-theme
+default-theme
+
+# Added neofetch on oh-my-zsh
+echo "neofetch" > /root/.oh-my-zsh/custom/example.zsh
+chmod +x /bin/neofetch
+neofetch
+
 # Check file system during boot
 uci set fstab.@global[0].check_fs=1
 uci commit
