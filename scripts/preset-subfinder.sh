58fbcde141237c07d3c27bbefaef2e773c2ffa06
Helmi Amirudin <helmilaw@gmail.com>
2022-07-27 17:18:45 +0700

    script: subfinder: add bugscanner-go

--- scripts/preset-subfinder.sh
+++ scripts/preset-subfinder.sh
@@ -5,29 +5,35 @@
 # System Required: Linux
 # Lisence: MIT
 # Author: Helmi Amirudin <https://www.helmiau.com>
-# Source: Subfinder by projectdiscovery <https://github.com/projectdiscovery/subfinder/>.
+#=================================================
+# Source: Subfinder by projectdiscovery <https://github.com/projectdiscovery/subfinder>.
+# Source: bugscanner-go by aztecrabbit <https://github.com/aztecrabbit/bugscanner-go>.
 #=================================================
 #
 #	Subfinder is a subdomain discovery tool that discovers valid subdomains for websites by using passive online sources.
 #	It has a simple modular architecture and is optimized for speed.
 #	subfinder is built for doing one thing only - passive subdomain enumeration, and it does that very well.
 #
+#	Bugscanner-go is useful tool for scanning a domain bug for Internet Freedom
+#
 #=================================================
 
 #=================================================
 # platform lists informations
 #=================================================
-# armv6 = arm_arm (rpi1,2,orange pi zero)
-# arm64 = aarch64 (rpi3,4 nanopi r2c,r2s,r4s, orange pi r1 plus)
-# amd64 = x86 64bit
 # 386 = i386/x86 32bit/generic
+# amd64 = x86 64bit
+# arm64 = aarch64 (rpi3,4 nanopi r2c,r2s,r4s, orange pi r1 plus)
+# armv5 = arm_arm (rpi1,2,orange pi zero)
+# armv6 = arm_arm (rpi1,2,orange pi zero)
+# armv7 = arm_arm (rpi1,2,orange pi zero)
 #=================================================
 
 #=================================================
 # Add Subfinder binary
 #=================================================
-SUBFDR_TAG=$(curl -sL https://api.github.com/repos/projectdiscovery/subfinder/releases | jq -r '.[0] | .tag_name' | sed 's/v//g')
-SUBFDR_REL="https://github.com/projectdiscovery/subfinder/releases/download/v${SUBFDR_TAG}/subfinder_${SUBFDR_TAG}_linux_$1.zip"
+SUBFDR_TAG=$(curl -sL https://api.github.com/repos/helmiau/subfinder/releases | jq -r '.[0] | .tag_name' | sed 's/v//g')
+SUBFDR_REL="https://github.com/helmiau/subfinder/releases/download/v${SUBFDR_TAG}/subfinder_${SUBFDR_TAG}_linux_$1.zip"
 SUBFDR_PATH="files/bin"
 SUBFDR_BIN="subfinder"
 
@@ -44,4 +50,27 @@ wget -qO- "$SUBFDR_REL" | gunzip -c > $SUBFDR_PATH/$SUBFDR_BIN
 echo -e "  Installing...."
 
 chmod +x $SUBFDR_PATH/$SUBFDR_BIN
+echo -e "  Done...."
+
+#=================================================
+# Add bugscanner-go binary
+#=================================================
+BUGSC_TAG=$(curl -sL https://api.github.com/repos/helmiau/bugscanner-go/releases | jq -r '.[0] | .tag_name' | sed 's/v//g')
+BUGSC_REL="https://github.com/helmiau/bugscanner-go/releases/download/v${BUGSC_TAG}/bugscanner-go_${BUGSC_TAG}_linux_$1.zip"
+BUGSC_PATH="files/bin"
+BUGSC_BIN="bugscanner-go"
+
+echo -e "  Installing bugscanner-go...."
+echo -e "  Architecture detected : $1...."
+
+# Check files/bin directory
+if [[ ! -d $BUGSC_PATH ]]; then
+	mkdir -p $BUGSC_PATH
+fi
+
+echo -e "  Downloading bugscanner-go v$BUGSC_TAG for linux $1 architecture...."
+wget -qO- "$BUGSC_REL" | gunzip -c > $BUGSC_PATH/$BUGSC_BIN
+echo -e "  Installing...."
+
+chmod +x $BUGSC_PATH/$BUGSC_BIN
 echo -e "  Done...."
