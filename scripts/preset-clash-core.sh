14eb5e13df2940413ad1e828397fde4442bbbff4
Helmi Amirudin <helmilaw@gmail.com>
2022-08-11 12:15:31 +0700

    Update preset-clash-core.sh

--- scripts/preset-clash-core.sh
+++ scripts/preset-clash-core.sh
@@ -56,7 +56,7 @@ wget -qO- $clash_tun_dreamacro | gunzip -c > $COREDIR/clash_tun_dreamacro
 # Docs: https://github.com/djoeni/Clash.Meta
 if [[ $1 == "386" ]]; then
 	clash_meta_wss_url=$(curl -sL $APIGIT/djoeni/Clash.Meta/releases/tags/Prerelease-WSS | grep Prerelease-WSS/Clash.Meta-linux-amd64-compatible | sed -e 's|"||g' -e 's| ||g' -e 's|browser_download_url:||g')
-if [[ $1 == "armv8" ]]; then
+elif [[ $1 == "armv8" ]]; then
 	clash_meta_wss_url=$(curl -sL $APIGIT/djoeni/Clash.Meta/releases/tags/Prerelease-WSS | grep Prerelease-WSS/Clash.Meta-linux-arm64 | sed -e 's|"||g' -e 's| ||g' -e 's|browser_download_url:||g')
 else
 	clash_meta_wss_url=$(curl -sL $APIGIT/djoeni/Clash.Meta/releases/tags/Prerelease-WSS | grep Prerelease-WSS/Clash.Meta-linux-$1 | sed -e 's|"||g' -e 's| ||g' -e 's|browser_download_url:||g')
