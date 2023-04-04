0941be8cbdc622ff1042c9ec4b36df31564885b8
Helmi Amirudin <helmilaw@gmail.com>
2023-04-05 04:17:49 +0700

    speedtest-cli: always fetch latest version

--- scripts/preset-speedtest-cli.sh
+++ scripts/preset-speedtest-cli.sh
@@ -20,15 +20,13 @@
 #=================================================
 # URL format sample for aarch64 with speedtest version
 #=================================================
-# Version: 1.1.0 <https://install.speedtest.net/app/cli/ookla-speedtest-1.1.0-aarch64-linux.tgz>
-# Version: 1.1.1 <https://install.speedtest.net/app/cli/ookla-speedtest-1.1.1-linux-aarch64.tgz>
-# Version: 1.2.0 <https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-i386.tgz>
+# Always fetch latest version $(curl -s https://www.speedtest.net/apps/cli | sed -e "s|<a href=|\n|g" -e "s|class=|\n|g" | grep 'https://install.speedtest.net/app/cli/ookla-speedtest-.*.-linux' | cut -d'"' -f2 | grep ${1})
 
 #=================================================
 # Add SpeedTest CLI Core by Ookla
 #=================================================
-SPDTVER="1.2.0"
-SPDTURL="https://install.speedtest.net/app/cli/ookla-speedtest-$SPDTVER-linux-$1.tgz"
+SPDTURL=$(curl -s https://www.speedtest.net/apps/cli | sed -e "s|<a href=|\n|g" -e "s|class=|\n|g" | grep 'https://install.speedtest.net/app/cli/ookla-speedtest-.*.-linux' | cut -d'"' -f2 | grep ${1})
+SPDTGZ=$(echo -e "${SPDTURL}" | sed "s|https://install.speedtest.net/app/cli/||g")
 SPDTDIR="files/bin"
 SPDTBIN="speedtest"
 echo -e "  Installing Speedtest Core by Ookla...."
@@ -39,7 +37,7 @@ if [[ ! -d $SPDTDIR ]]; then
 	mkdir -p $SPDTDIR
 fi
 
-echo -e "  Downloading ookla-speedtest-$SPDTVER-$1-linux.tgz...."
+echo -e "  Downloading ${SPDTGZ}...."
 wget -qO- $SPDTURL | tar xOvz > $SPDTDIR/$SPDTBIN
 echo -e "  Installing...."
 
